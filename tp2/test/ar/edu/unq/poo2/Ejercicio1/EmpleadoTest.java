9ffd627d286cf56f94d70b9c8ecd4b0c48f54fac
Leandro <leandroagustin.cuevas@gmail.com>
2023-08-31 23:05:46 -0300

    Tests

--- tp2/test/ar/edu/unq/poo2/Ejercicio1/EmpleadoTest.java
+++ tp2/test/ar/edu/unq/poo2/Ejercicio1/EmpleadoTest.java
@@ -12,48 +12,46 @@ class EmpleadoTest {
 	private EmpleadoPermanente empleadopermanente;
 	private EmpleadoContratado empleadocontratado;
 	private Empresa empresa;
-	
+
 	@BeforeEach
 	void setUp() {
-				
-		empleadotemporal = new EmpleadoTemporario("Leandro", "Evergreen Avenue", "Soltero", LocalDate.of(1930, 8, 16), 4);
+
+		empleadotemporal = new EmpleadoTemporario("Leandro", "Evergreen Avenue", "Soltero", LocalDate.of(1930, 8, 16),
+				4);
 		empleadopermanente = new EmpleadoPermanente("Nacho", "Fake Street", "Casado", LocalDate.of(1990, 6, 23), 2, 10);
-		empleadocontratado = new EmpleadoContratado("Rodri", "Quilmes Road", "Viudo", LocalDate.of(2001, 7, 30), "0120230101", "Efectivo");
+		empleadocontratado = new EmpleadoContratado("Rodri", "Quilmes Road", "Viudo", LocalDate.of(2001, 7, 30),
+				"0120230101", "Efectivo");
 		empresa = new Empresa("Empresario Feliz", "17121234562");
 		empresa.contratarTrabajador(empleadotemporal);
 		empresa.contratarTrabajador(empleadopermanente);
 		empresa.contratarTrabajador(empleadocontratado);
 
 	}
-	
+
 	@Test
 	void valoresDelTemporario() {
 		assertEquals(1160, empleadotemporal.sueldoBruto());
 		assertEquals(277, empleadotemporal.retenciones());
 		assertEquals(883, empleadotemporal.sueldoNeto());
 		assertEquals(empleadotemporal.desgloceDeConceptos(),
-			"Sueldo bruto está compuesto por:\n"+
-			"Sueldo básico: $1000\n" +
-			"Horas extras: $160\n" +
-			"Se descuenta en calidad de retenciones:\n" +
-			"Obra social: $141\n" +
-			"Jubilación: $136");
+				"Sueldo bruto está compuesto por:\n" + "Sueldo básico: $1000\n" + "Horas extras: $160\n"
+						+ "Se descuenta en calidad de retenciones:\n" + "Obra social: $141\n" + "Jubilación: $136");
 	}
-	
+
 	@Test
 	void valoresDelPermanente() {
 		assertEquals(1900, empleadopermanente.sueldoBruto());
 		assertEquals(515, empleadopermanente.retenciones());
 		assertEquals(1385, empleadopermanente.sueldoNeto());
 	}
-	
+
 	@Test
 	void valoresDelContratado() {
 		assertEquals(1000, empleadocontratado.sueldoBruto());
 		assertEquals(50, empleadocontratado.retenciones());
 		assertEquals(950, empleadocontratado.sueldoNeto());
 	}
-	
+
 	@Test
 	void testEnEmpresa() {
 		assertEquals(3, empresa.getEmpleados().size());
@@ -63,5 +61,4 @@ class EmpleadoTest {
 		empresa.realizarLiqDeSueldos();
 		assertEquals(3, empresa.getRecibos().size());
 	}
-
 }
